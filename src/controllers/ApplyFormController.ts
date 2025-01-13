f6d14ed7adea7c290e45bcf883555dd7e60fa2bb
dylanBkainos <dylanBkainos@users.noreply.github.com>
2025-01-13 10:25:37 +0000

    Prettified Code!

--- src/controllers/ApplyFormController.ts
+++ src/controllers/ApplyFormController.ts
@@ -1,10 +1,9 @@
-import express from 'express';
+import express from "express";
 
 export const getJobForm = async (req: express.Request, res: express.Response): Promise<void> => {
-  res.render('jobRole/job-form.njk');
-}
+	res.render("jobRole/job-form.njk");
+};
 
 export const postJobForm = async (req: express.Request, res: express.Response): Promise<void> => {
-  console.log(req.file)
-}
-
+	console.log(req.file);
+};
