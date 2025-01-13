272d369cc2df40dcf657ec250e79856554d9f274
dandabs <dandabs@users.noreply.github.com>
2025-01-13 09:09:37 +0000

    Prettified Code!

--- test/unit/controllers/AuthControllerTests.ts
+++ test/unit/controllers/AuthControllerTests.ts
@@ -4,53 +4,52 @@ import sinon from "sinon";
 import * as authController from "../../../src/controllers/AuthController";
 
 describe("AuthController", () => {
-    let req, res;
-  
-    beforeEach(() => {
-      req = {
-        body: {},
-        session: {},
-      };
-      res = {
-        render: sinon.stub(),
-        redirect: sinon.stub(),
-        locals: {},
-      };
-    });
-  
-    afterEach(() => {
-      sinon.restore();
-    });
-  
-    describe("getLoginForm", () => {
-      it("should render the login form", async () => {
-        await authController.getLoginForm(req, res);
-        expect(res.render.calledWith("auth/login.njk")).to.be.true;
-      });
-    });
-  
-    describe("postLoginForm", () => {
-      it("should render the login form with an error message on failure", async () => {
-        const errorMessage = "Login error:";
-  
-        req.body = { email: "test@example.com", password: "password" };
-  
-        await authController.postLoginForm(req, res);
-  
-        expect(res.locals.errormessage).to.contain(errorMessage);
-        expect(res.render.calledWith("auth/login.njk", req.body)).to.be.true;
-      });
-    });
-  
-    describe("getLogoutForm", () => {
-      it("should destroy the session and redirect to the homepage", async () => {
-        req.session.destroy = sinon.stub().callsFake((callback) => callback());
-  
-        await authController.getLogoutForm(req, res);
-  
-        expect(req.session.destroy.calledOnce).to.be.true;
-        expect(res.redirect.calledWith("/")).to.be.true;
-      });
-    });
-  });
-  
+	let req, res;
+
+	beforeEach(() => {
+		req = {
+			body: {},
+			session: {},
+		};
+		res = {
+			render: sinon.stub(),
+			redirect: sinon.stub(),
+			locals: {},
+		};
+	});
+
+	afterEach(() => {
+		sinon.restore();
+	});
+
+	describe("getLoginForm", () => {
+		it("should render the login form", async () => {
+			await authController.getLoginForm(req, res);
+			expect(res.render.calledWith("auth/login.njk")).to.be.true;
+		});
+	});
+
+	describe("postLoginForm", () => {
+		it("should render the login form with an error message on failure", async () => {
+			const errorMessage = "Login error:";
+
+			req.body = { email: "test@example.com", password: "password" };
+
+			await authController.postLoginForm(req, res);
+
+			expect(res.locals.errormessage).to.contain(errorMessage);
+			expect(res.render.calledWith("auth/login.njk", req.body)).to.be.true;
+		});
+	});
+
+	describe("getLogoutForm", () => {
+		it("should destroy the session and redirect to the homepage", async () => {
+			req.session.destroy = sinon.stub().callsFake((callback) => callback());
+
+			await authController.getLogoutForm(req, res);
+
+			expect(req.session.destroy.calledOnce).to.be.true;
+			expect(res.redirect.calledWith("/")).to.be.true;
+		});
+	});
+});
--- test/unit/services/AuthServiceTests.ts
+++ test/unit/services/AuthServiceTests.ts
@@ -5,41 +5,40 @@ import MockAdapter from "axios-mock-adapter";
 import { getToken } from "../../../src/services/AuthService";
 import { LoginRequest } from "../../../src/models/LoginRequest";
 
-
 describe("AuthService", () => {
-  let mockAxios;
+	let mockAxios;
 
-  beforeEach(() => {
-    process.env.API_URL = "http://localhost:8080/api"
-    mockAxios = new MockAdapter(axios);
-  });
+	beforeEach(() => {
+		process.env.API_URL = "http://localhost:8080/api";
+		mockAxios = new MockAdapter(axios);
+	});
 
-  afterEach(() => {
-    mockAxios.restore();
-  });
+	afterEach(() => {
+		mockAxios.restore();
+	});
 
-  it("should return a token when API call succeeds", async () => {
-    const loginRequest: LoginRequest = { email: "test@example.com", password: "password" };
-    const mockToken = "mockToken123";
+	it("should return a token when API call succeeds", async () => {
+		const loginRequest: LoginRequest = { email: "test@example.com", password: "password" };
+		const mockToken = "mockToken123";
 
-    mockAxios.onPost(`${process.env.API_URL}/auth/login`).reply(200, mockToken);
+		mockAxios.onPost(`${process.env.API_URL}/auth/login`).reply(200, mockToken);
 
-    const result = await getToken(loginRequest);
-    expect(result).to.equal(mockToken);
-  });
+		const result = await getToken(loginRequest);
+		expect(result).to.equal(mockToken);
+	});
 
-  it("should throw an error when invalid credentials", async () => {
-    const loginRequest = { email: "test@example.com", password: "password" };
-    const mockError = "Invalid credentials";
-    const config = {url: `${process.env.API_URL}/auth/login`} as InternalAxiosRequestConfig
-    const errorResponse = new AxiosError("Invalid credentials", "lol", config, loginRequest)
-    mockAxios.onPost(`${process.env.API_URL}/auth/login`).reply(400, errorResponse);
+	it("should throw an error when invalid credentials", async () => {
+		const loginRequest = { email: "test@example.com", password: "password" };
+		const mockError = "Invalid credentials";
+		const config = { url: `${process.env.API_URL}/auth/login` } as InternalAxiosRequestConfig;
+		const errorResponse = new AxiosError("Invalid credentials", "lol", config, loginRequest);
+		mockAxios.onPost(`${process.env.API_URL}/auth/login`).reply(400, errorResponse);
 
-    try {
-      await getToken(loginRequest);
-    } catch (e) {
-      console.log(e)
-      expect(e.message).to.equal(mockError);
-    }
-  });
+		try {
+			await getToken(loginRequest);
+		} catch (e) {
+			console.log(e);
+			expect(e.message).to.equal(mockError);
+		}
+	});
 });
