a91f50065a26981101299a021b429ff5b403a055
CharlotteMKainos <charlotte.mcknight@kainos.com>
2025-01-14 13:19:32 +0000

    updated package json file, added aws envs, removed strict in config file, created mocha json file

--- src/app.ts
+++ src/app.ts
@@ -5,60 +5,64 @@ import session from "express-session";
 import { logger } from "./logger";
 import { getLoginForm, getLogoutForm, postLoginForm } from "./controllers/AuthController";
 import jobRoleMiddleware from "./middleware/JobRoleMiddleware";
-import { getAllJobRolesList } from "./controllers/JobRoleController";
-import { getDetailedJobRoleController } from "./controllers/JobRoleController";
+import { getAllJobRolesList, getDetailedJobRoleController } from "./controllers/JobRoleController";
 import { getJobForm, postJobForm } from "./controllers/ApplyFormController";
 import multer from "multer";
 import multerS3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
 import { v4 as uuid } from "uuid";
-require("dotenv").config();
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
+const bucketName = process.env.BUCKET_NAME || 'default-bucket-name';
+const sessionSecret = process.env.SESSION_SECRET || 'default-session-secret';
+const sessionMaxAge = parseInt(process.env.SESSION_MAX_AGE || '28800000');
+
 const s3 = new S3Client({
-	region: process.env.AWS_REGION,
-	credentials: {
-		accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "default  string",
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "default  string",
-	},
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "default string",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "default string",
+  },
 });
 
 const upload = multer({
-	storage: multerS3({
-		s3,
-		bucket: process.env.BUCKET_NAME,
-		metadata: function (req, file, callback) {
-			callback(null, { fieldName: file.fieldname });
-		},
-		key: function (req, file, callback) {
-			callback(null, uuid());
-		},
-	}),
+  storage: multerS3({
+    s3,
+    bucket: bucketName,
+    metadata: function (req, file, callback) {
+      callback(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, callback) {
+      callback(null, uuid());
+    },
+  }),
 });
 
+app.use(session({
+  secret: sessionSecret,
+  cookie: { maxAge: sessionMaxAge }
+}));
+
 app.use(bodyParser.json());
-app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const env = nunjucks.configure(["node_modules/govuk-frontend/dist", "views"], {
-	autoescape: true,
-	express: app,
+  autoescape: true,
+  express: app,
 });
 
 app.listen(process.env.port || 3000, () => {
-	logger.info(`Application listening on port ${process.env.port || 3000}`);
+  logger.info(`Application listening on port ${process.env.port || 3000}`);
 });
 
-app.use(session({ secret: process.env.SESSION_SECRET, cookie: { maxAge: parseInt(process.env.SESSION_MAX_AGE) || 28800000 } }));
-
 declare module "express-session" {
-	interface SessionData {
-		token: string;
-	}
+  interface SessionData {
+    token: string;
+  }
 }
 
 app.use(express.static("node_modules/govuk-frontend/dist/govuk/"));
@@ -66,12 +70,12 @@ app.use(express.static("node_modules/govuk-frontend/dist/govuk/assets"));
 app.use(express.static("static/"));
 
 app.use((req, res, next) => {
-	env.addGlobal("request", req);
-	next();
+  env.addGlobal("request", req);
+  next();
 });
 
 app.get("/", function (req, res) {
-	res.render("index.njk");
+  res.render("index.njk");
 });
 
 app.get("/job-form", getJobForm);
@@ -86,5 +90,5 @@ app.get("/job-roles", jobRoleMiddleware, getAllJobRolesList);
 app.get("/job-roles/:id", jobRoleMiddleware, getDetailedJobRoleController);
 
 app.get("*", function (req, res) {
-	res.render("errors/404.njk");
+  res.render("errors/404.njk");
 });
